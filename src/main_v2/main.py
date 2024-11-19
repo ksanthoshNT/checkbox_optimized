6cd6cf86cb59ecd6a208081fdca838295dca200f
kammarisanthosh <kammari.santhosh@newgensoft.com>
2024-11-19 18:42:15 +0530

    added path

--- src/main_v2/main.py
+++ src/main_v2/main.py
@@ -8,6 +8,8 @@ import numpy as np
 import logging
 from pathlib import Path
 
+from tqdm import tqdm
+
 from src.main_v2.preprocessor import ImagePreprocessor
 
 logger = logging.getLogger(__name__)
@@ -98,8 +100,8 @@ class ImagePatternMatcher:
 
 
     def is_box(self,x1:int,y1:int,x2:int,y2:int):
-        return (self.get_iloc(self.image_arr_x_cumsum,x2,y1)-self.get_iloc(self.image_arr_x_cumsum,x1,y1) == self.get_iloc(self.image_arr_x_cumsum,x2,y2)-self.get_iloc(self.image_arr_x_cumsum,x1,y2)) and (
-            self.get_iloc(self.image_arr_y_cumsum,x1,y2)-self.get_iloc(self.image_arr_y_cumsum,x2,y1) == self.get_iloc(self.image_arr_y_cumsum, x2, y2) -self.get_iloc(self.image_arr_y_cumsum, x2, y1)
+        return (self.get_iloc(self.image_arr_x_cumsum,x2,y1)-(self.get_iloc(self.image_arr_x_cumsum,x1-1,y1) if x1-1>=0 else 0) == self.get_iloc(self.image_arr_x_cumsum,x2,y2)-(self.get_iloc(self.image_arr_x_cumsum,x1-1,y2) if x1-1>=0 else 0)) and (
+            self.get_iloc(self.image_arr_y_cumsum,x1,y2)-(self.get_iloc(self.image_arr_y_cumsum,x2,y1-1) if y1-1>=0 else 0) == self.get_iloc(self.image_arr_y_cumsum, x2, y2) -(self.get_iloc(self.image_arr_y_cumsum, x2, y1-1) if y1-1>=0 else 0)
         )
 
     def get_iloc(self,arr,x1,y1):
@@ -111,6 +113,46 @@ class ImagePatternMatcher:
             for y in range(mat.shape[1]):
                 print('1' if mat[x, y] == 1 else '0', end="")
             print()
+
+
+def get_indices_of_value(input_array: np.ndarray,val) -> np.ndarray:
+    """
+    Returns a numpy array with indices of the input array that are equal to 1.
+
+    Parameters:
+    input_array (np.ndarray): Input numpy array to find indices of 1.
+
+    Returns:
+    np.ndarray: Indices where input_array equals 1.
+    """
+    # Use np.where to get indices where input_array equals 1
+    indices = np.where(input_array == val)
+    return indices
+
+def get_rotate_and_imposed_indices(arr:np.ndarray):
+    new_rot_array = np.rot90(arr, k=2)
+    both_ones = (arr == 1) & (new_rot_array == 1)
+    indices = np.where(both_ones)
+    return indices
+
+
+# def list_based_boxes():
+#     boxes = []
+#     vals = np.sort(np_concatenated).tolist()
+#     for idx, x in enumerate(tqdm(vals, desc="X..")):
+#         for y in vals[idx:]:
+#             if matcher.is_box(x[0], x[1], y[0], y[1]):
+#                 boxes.append([x[0], x[1], y[0], y[1]])
+
+def get_merged_indices_from_image(image_arr=None):
+    indices = get_rotate_and_imposed_indices(image_arr)
+
+    print(indices[0].shape)  # shape (32494,)
+    print(indices[1].shape)  # shape (32494,)
+
+    np_concatenated = np.concatenate((indices[0].reshape(-1, 1), indices[1].reshape(-1, 1)), axis=1)
+    return np_concatenated
+
 
 def main(image_path: str, threshold: int = 125, confidence: float = 0.8):
     start_time = time.perf_counter()
@@ -125,28 +167,40 @@ def main(image_path: str, threshold: int = 125, confidence: float = 0.8):
 
     matcher.compute_cumsum_arrays(image_arr.copy())
 
-    print(matcher.is_box(0,0,2,2))
+    updated_image_indices = get_merged_indices_from_image(image_arr=image_arr)
+    #shape of updated_image_indices: (32494,2)
 
+    # boxes = []
+    # vals = np.sort(updated_image_indices).tolist()
+    # for idx,x in enumerate(tqdm(vals,desc="X..")):
+    #     for y in vals[idx:]:
+    #         if matcher.is_box(x[0],x[1],y[0],y[1]):
+    #             boxes.append([x[0],x[1],y[0],y[1]])
+    #
+    # print(len(boxes))
 
     end_time = time.perf_counter()
-    logger.info(f'Time: {end_time-start_time:.2f}s')
-    return
+    print(f"Duration: {end_time - start_time} seconds")
 
-
-def dummY():
-    ar = np.array([[1,1,1,1],
-                   [1,0,0,1],
-                   [1,0,0,1],
-                  [1,1,1,1]])
-    matcher = ImagePatternMatcher()
-    matcher.compute_cumsum_arrays(ar)
-    print(matcher.image_arr_x_cumsum)
-    print('=========')
-    print(matcher.image_arr_y_cumsum)
-    print(matcher.is_box(0,0,3,3))
 
 
 
 if __name__ == "__main__":
-    # main("/home/ntlpt59/MAIN/experiments/checkbox_optimized/data/Export-Bill_filled_sample0.jpg")
-    dummY()
+    main("/home/ntlpt59/MAIN/experiments/checkbox_optimized/data/Export-Bill_filled_sample0.jpg")
+
+
+####tests
+# def test_is_box():
+#     ar = np.array([[1,1,1,1],
+#                    [1,0,0,1],
+#                    [1,0,0,1],
+#                   [1,1,1,1]])
+#     matcher = ImagePatternMatcher()
+#     matcher.compute_cumsum_arrays(ar)
+#     print(matcher.image_arr_x_cumsum)
+#     print('=========')
+#     print(matcher.image_arr_y_cumsum)
+#     print(matcher.is_box(0,0,3,3))
+
+
+
---
+++ src/main_v2/practice.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Original array
+arr = np.array([[0, 1, 0],
+                [1, 0, 1],
+                [0, 0, 1],
+                [1,0,1]])
+
+
+
+print(get_rotate_and_imposed_indices(arr))
